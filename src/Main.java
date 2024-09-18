5759a328574b09eeb4bd04c1b2351741d141684d
Gustavo Sobrinho <gug.sobrinho0910@gmail.com>
2024-09-18 16:27:50 -0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Book> livros = new DatasetReader().readDataset("src/data.csv");
+        Book livro = new Book();
+
+        System.out.print(livro.numberOfBookByAuthor(livros, "J.K. Rowling"));
+        System.out.println("\n");
+        System.out.print(livro.listaAutor(livros));
+        System.out.println("\n");
+        livro.livrosDoAutor(livros, "J.K. Rowling");
+        System.out.println("\n");
+    }
+}
